0fe73981b3bfc6c3220f3d1142ef7256fa821748
halimcan <albayhalim@gmail.com>
2026-01-02 20:35:36 +0300

    Implement euclidean distance

--- utils/distances.py
+++ utils/distances.py
@@ -3,3 +3,8 @@ def manhattan(a, b):
     d_x = abs(b[0] - a[0])
     d_y = abs(b[1] - a[1])
     return d_x + d_y
+
+import math
+
+def euclidean(a, b):
+    return math.sqrt((b[0] - a[0])**2 + (b[1] - a[1])**2)
