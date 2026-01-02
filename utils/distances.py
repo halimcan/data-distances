4467661b7f4842029995e4febb2be0822e645a28
halimcan <albayhalim@gmail.com>
2026-01-02 20:14:27 +0300

    fix(utils): manhattan distance with abs

--- utils/distances.py
+++ utils/distances.py
@@ -1,6 +1,5 @@
 pass  # YOUR CODE HERE
-def manhattan(a,b):
-    d_x = b[0] - a[0]
-    d_y = b[1] - a[1]
-    distance = abs(d_x + d_y)
-    return distance
+def manhattan(a, b):
+    d_x = abs(b[0] - a[0])
+    d_y = abs(b[1] - a[1])
+    return d_x + d_y
